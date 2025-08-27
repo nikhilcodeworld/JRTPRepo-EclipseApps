039ee8cc849254afed16145e8b6a3807c0d1c014
Piyush <Piyush@DESKTOP-5GDIIUB>
2025-08-27 20:32:17 +0530

    initial commit from TL

---
+++ GitProj01/src/main/java/com/nt/main/GitProj01Application.java
@@ -0,0 +1,13 @@
+package com.nt.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitProj01Application.class, args);
+	}
+
+}
